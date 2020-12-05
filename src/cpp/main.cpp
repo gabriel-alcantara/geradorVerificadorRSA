1980f3d0f18444c7313828da42b33e7a636d5dd6
gabriel <gabrielalcantara@wizsolucoes.com.br>
2020-12-05 18:31:26 -0300

    initilizing project

---
+++ src/cpp/main.cpp
@@ -0,0 +1,16 @@
+// Your First C++ Program
+
+#include <iostream>
+
+// #include <openssl/bio.h> /* BasicInput/Output streams */
+
+// #include <openssl/err.h> /* errors */
+
+// #include <openssl/ssl.h> /* core library */
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    return 0;
+}
